59295cf4a0003b0c6b862b9803bb98b338dde231
jbaem <101052774+jbaem@users.noreply.github.com>
2023-05-07 00:20:18 +0900

    init

---
+++ mcku.h
@@ -0,0 +1,34 @@
+struct pcb{
+	char pid;
+	FILE *fd;
+	char *pgtable;
+	/* Add more fields if needed */
+};
+
+
+void ku_scheduler(char pid){
+
+	/* Your code here */
+
+}
+
+
+void ku_pgfault_handler(char pid){
+
+	/* Your code here */
+
+}
+
+
+void ku_proc_exit(char pid){
+
+	/* Your code here */
+
+}
+
+
+void ku_proc_init(int nprocs, char *flist){
+
+	/* Your code here */
+
+}
