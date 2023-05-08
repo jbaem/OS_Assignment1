dbdfd0ec6c8847d08cfb4a689173c1ec2e56df31
jbaem <101052774+jbaem@users.noreply.github.com>
2023-05-09 02:54:03 +0900

    update

--- mcku.h
+++ mcku.h
@@ -2,13 +2,13 @@
 #include<stdbool.h>
 #define MAX_NAME_LENGTH 256
 #define PTE_COUNT 16
+#define FREE_LIST_SIZE 64
 
 struct pcb{
 	char pid;
 	FILE *fd;
 	char *pgtable;
 	bool isEnd;
-	bool *freeList;
 };
 
 char *deleteLF(char * str); //delete Line Feed
@@ -19,6 +19,8 @@ extern char *ptbr;
 
 /*global*/
 struct pcb* pcbs;
+char *freeList;
+
 int nProcess;
 int restProcess;
 
@@ -26,7 +28,7 @@ void ku_scheduler(char pid){
 	/*find remain jobs*/
 	while(restProcess > 0) {
 		current = &pcbs[++pid % nProcess];
-		/*current is not null*/
+		/*current is not finished*/
 		if(!(current->isEnd)) {
 			ptbr = current->pgtable;
 			return;
@@ -42,7 +44,7 @@ void ku_scheduler(char pid){
 
 void ku_pgfault_handler(char va) {
 	int pt_index = (va & 0xF0) >> 4;
-	ptbr[pt_index] = 1;
+	*(ptbr + pt_index) = 1;
 	return;
 }
 
@@ -51,7 +53,6 @@ void ku_proc_exit(char pid){
 	/*free pcbs[pid]*/
 	fclose(pcbs[pid].fd);
 	free(pcbs[pid].pgtable);
-	free(pcbs[pid].freeList);
 	pcbs[pid].isEnd = true;
 	restProcess--;
 
@@ -71,11 +72,12 @@ void ku_proc_init(int nprocs, char *flist){
 		exit(0);
 	}
 
-	/*allocate pcbs for nprocs*/
+	/*allocate pcbs & free list*/
 	nProcess = nprocs;
 	restProcess = nprocs;
 	pcbs = malloc(sizeof(struct pcb) * nprocs);
-	
+	freeList = malloc(FREE_LIST_SIZE);
+
 	for(int pi = 0; pi < nProcess; ++pi) {
 		/*read each text file's name*/
 		char *procFile = NULL;
@@ -96,8 +98,6 @@ void ku_proc_init(int nprocs, char *flist){
 		pcbs[pi].pid = pi;
 		pcbs[pi].pgtable = malloc(sizeof(pcbs->pgtable) * PTE_COUNT);
 		pcbs[pi].isEnd = false;
-		pcbs[pi].freeList = malloc(sizeof(bool) * PTE_COUNT);
-		for(int i = 0; i < PTE_COUNT; ++i) pcbs[pi].freeList[i] = true;
 
 		/*free character pointer*/
 		free(procFile);
