1ab4cfc51c2aff5016d5845c2cfecd6bf487ea25
jbaem <101052774+jbaem@users.noreply.github.com>
2023-05-08 16:38:32 +0900

    getline에서 fgets로

--- mcku.h
+++ mcku.h
@@ -1,5 +1,6 @@
 #include<string.h>
 #include<stdbool.h>
+#define MAX_NAME 256
 
 char *deleteNewLine(char *str);
 
@@ -7,26 +8,24 @@ struct pcb{
 	char pid;
 	FILE *fd;
 	char *pgtable;
+	
 	/* Add more fields if needed */
 	bool isExit;
 };
 
-// pcb 배열
-struct pcb* pcbs;
-// 프로세스 개수
-int processLength;
-// mcku.c에서 사용하는 변수 가져오기 위한 선언
+// mcku.c 외부 변수
 extern struct pcb *current;
 extern char *ptbr;
 
+struct pcb* pcbs;
+int nProcess;
+
 // pid  1씩 올려서 current에 넣는 방식
 void ku_scheduler(char pid){
-	// 무한루프 막기 위해서 count 변수 선언
-	int count = 0;
-
+	
 	do {
 		current = &ptbr[++pid % processLength];
-		ptbr = current -> pgtable;
+		ptbr = current->pgtable;
 	} while(current -> isExit && count++ < processLength);
 	
 	// 만약 모든 프로세스가 exit 됐다면 프로그램 종료
@@ -48,25 +47,26 @@ void ku_proc_exit(char pid){
 
 
 void ku_proc_init(int nprocs, char *flist){
-	// 프로세스 개수 기억
-	processLength = nprocs;
-	// 프로세스 파일 리스트 들어이쓴 파일 불러오기
-	FILE *fileList = fopen(flist, "r");
-	// 프로세스 개수만큼 pcb 동적할당
-	pcbs = malloc(sizeof * pcbs * nprocs);
-	// 파일 줄 단위로 읽을 때 필요함 - 하지만 안 씀
-	size_t len = 0;
+	nProcess = nprocs;
+	pcbs = malloc(sizeof(struct pcb) * nprocs);
+	
+	FILE *fp = fopen(flist, "r");
+	if(fp == NULL) {
+		printf("error - file open");
+		exit(0);
+	}
 
-	for(int i=0; i<nprocs; ++i) {
+	for(int i = 0; i < nprocs; ++i) {
 		// 프로세스 파일명 읽어오기
-		char *processFileName = NULL;
-		getline(&processFileName, &len, fileList);
+		char *procFile = NULL;
+		fgets(procFile, MAX_NAME, fp);
+		
 		//개행문자 지워주는 과정
-		processFileName = deleteNewLine(processFileName);
+		//processFileName = deleteNewLine(processFileName);
 
-		pcbs[i].fd = fopen(processFileName, "r");
+		pcbs[i].fd = fopen(procFile, "r");
 		pcbs[i].pid = i;
-		pcbs[i].pgtable = malloc(sizeof *pcbs->pgtable *16);
+		pcbs[i].pgtable = malloc(sizeof(pcbs->pgtable) *16);
 		pcbs[i].isExit = false;
 
 		free(processFileName);
