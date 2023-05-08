a19ac71a6a9f9bab8a50a9f996372f53740c5016
jbaem <101052774+jbaem@users.noreply.github.com>
2023-05-08 16:48:44 +0900

    Update mcku.c

--- mcku.c
+++ mcku.c
@@ -95,11 +95,13 @@ void ku_run_cpu(void){
 int main(int argc, char *argv[]){
 	/* System initialization */
 	ku_os_init();
-
+	printf("\nos_init success");
 	/* Per-process initialization */
 	ku_proc_init(atoi(argv[1]), argv[2]);
+	printf("\nproc_init success");
 	/* Process execution */
 	ku_run_cpu();
-
+	printf("\nrun_cpu success");
+	
 	return 0;
 }
