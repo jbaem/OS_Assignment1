a2de8accdca3470d175d6e8770594f44104b04c8
jbaem <101052774+jbaem@users.noreply.github.com>
2023-05-08 17:05:09 +0900

    Update mcku.h

--- mcku.h
+++ mcku.h
@@ -54,7 +54,7 @@ void ku_proc_init(int nprocs, char *flist){
 	nProcess = nprocs;
 	pcbs = malloc(sizeof(struct pcb) * nprocs);
 	
-	printf("\nstart file open");
+	printf("\nstart file open %s", flist);
 	FILE *fp = fopen(flist, "r");
 	if(fp == NULL) {
 		printf("error - file open");
